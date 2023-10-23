45f8b2dcdb2410a4fc3ed104bdfe55811079cf6a
phankons <stephen.han@gmx.com>
2023-10-23 14:10:38 -0400

    initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-        cssnano:
-            process.env.NPM_ENV === 'production'
-                ? { preset: 'default' }
-                : false,
-    },
+  plugins: {
+    autoprefixer: {},
+    tailwindcss: {},
+  },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,81 +1,13 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
 module.exports = {
-    content: [
-        './layouts/**/*.html',
-        './content/**/*.md',
-        './content/**/*.html',
-        './src/**/*.ts',
-        './src/**/*.js',
-    ],
-    darkMode: 'class',
-    theme: {
-        extend: {
-            // sidebar mobile fix
-            maxWidth: {
-                '2xs': '16rem',
-                '8xl': '90rem',
-            },
-            fontFamily: {
-                sans: [
-                    'Inter',
-                    'ui-sans-serif',
-                    'system-ui',
-                    '-apple-system',
-                    'system-ui',
-                    'Segoe UI',
-                    'Roboto',
-                    'Helvetica Neue',
-                    'Arial',
-                    'Noto Sans',
-                    'sans-serif',
-                    'Apple Color Emoji',
-                    'Segoe UI Emoji',
-                    'Segoe UI Symbol',
-                    'Noto Color Emoji',
-                ],
-                body: [
-                    'Inter',
-                    'ui-sans-serif',
-                    'system-ui',
-                    '-apple-system',
-                    'system-ui',
-                    'Segoe UI',
-                    'Roboto',
-                    'Helvetica Neue',
-                    'Arial',
-                    'Noto Sans',
-                    'sans-serif',
-                    'Apple Color Emoji',
-                    'Segoe UI Emoji',
-                    'Segoe UI Symbol',
-                    'Noto Color Emoji',
-                ],
-                mono: [
-                    'ui-monospace',
-                    'SFMono-Regular',
-                    'Menlo',
-                    'Monaco',
-                    'Consolas',
-                    'Liberation Mono',
-                    'Courier New',
-                    'monospace',
-                ],
-            },
-            fontSize: {
-                '2xs': '0.625rem',
-            },
-        },
-    },
-    variants: {
-        extend: {
-            backgroundColor: ['active'],
-        },
-    },
-    plugins: [
-        require('./plugin')({
-            charts: true,
-            forms: true,
-            tooltips: true,
-        }),
-    ],
+  content: [
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/flowbite-react/**/*.js",
+    "./public/**/*.html",
+    "./app/**/*.{ts,tsx}",
+  ],
+  plugins: [require("flowbite/plugin")],
+  theme: {},
 };
