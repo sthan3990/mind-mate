ce830f396b2519365bb3832f938bf7fae5c7705e
phankons <stephen.han@gmx.com>
2023-10-23 23:01:35 -0400

    Fix Error: `'` can be escaped with `&apos;`, `&lsquo;`, `&#39;`, `&rsquo;`

--- src/app/register/termsmodal.tsx
+++ src/app/register/termsmodal.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from 'react';
 import {
@@ -21,22 +21,22 @@ function TermsModal({ isOpen, onClose, onAgree }) {
         <ModalCloseButton />
         <ModalBody>
           <p>
-            Creating terms and conditions for a Software as a Service (SaaS) product is a complex legal task, and it's highly recommended to consult with a legal professional or attorney who specializes in technology and SaaS agreements. These terms need to address various aspects of the service, user rights, data privacy, and legal compliance. Below is a generic outline of terms that are often included in SaaS terms and conditions:
+            Creating terms and conditions for a Software as a Service (SaaS) product is a complex legal task, and it&#46;s highly recommended to consult with a legal professional or attorney who specializes in technology and SaaS agreements. These terms need to address various aspects of the service, user rights, data privacy, and legal compliance. Below is a generic outline of terms that are often included in SaaS terms and conditions:
           </p>
           <br />
-          <strong>1. Introduction:</strong>
+          <strong>1&#46; Introduction:</strong>
           <ul>
             <li>Explanation of the agreement and its parties.</li>
             <li>Brief overview of the SaaS service provided.</li>
           </ul>
 
-          <strong>2. Acceptance of Terms:</strong>
+          <strong>2&#46; Acceptance of Terms:</strong>
           <ul>
-            <li>User's agreement to the terms and conditions.</li>
+            <li>User&#46;s agreement to the terms and conditions.</li>
             <li>The age restriction (if applicable).</li>
           </ul>
 
-          <strong>3. Contact Information:</strong>
+          <strong>3&#46; Contact Information:</strong>
           <ul>
             <li>Contact information for user inquiries and support.</li>
           </ul>
