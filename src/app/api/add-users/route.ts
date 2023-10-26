52a070b88bfa07d4ab51f70bed95c53a405e95e3
William Kemeny <williamkemeny@gmail.com>
2023-10-26 10:08:48 -0400

    added bcrypt

--- src/app/api/add-users/route.ts
+++ src/app/api/add-users/route.ts
@@ -2,6 +2,8 @@ import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 import { redirect } from "next/navigation";
 
+const bcrypt = require("bcrypt");
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const fName = searchParams.get("fName");
@@ -9,12 +11,18 @@ export async function GET(request: Request) {
   const email = searchParams.get("email");
   const password = searchParams.get("password");
 
+  const salt = bcrypt.genSaltSync(8);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+
   try {
     if (!fName || !email || !password)
       throw new Error("First name, email, and password is required");
-    await sql`INSERT INTO users (first_name, last_name, email, password) VALUES (${fName}, ${lName}, ${email}, ${password});`;
+    await sql`INSERT INTO users (first_name, last_name, email, password) VALUES (${fName}, ${lName}, ${email}, ${hashedPassword});`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
-  redirect("/");
+  //redirect("/");
+
+  const users = await sql`SELECT * FROM users;`;
+  return NextResponse.json({ users: users.rows }, { status: 200 });
 }
