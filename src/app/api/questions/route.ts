d27773a5dc024ee2126baa9d71e9a8cff1b2b839
William Kemeny <williamkemeny@gmail.com>
2023-10-31 18:07:50 -0400

    DB quaries for responses and answers created

---
+++ src/app/api/questions/route.ts
@@ -0,0 +1,50 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const { journalId, question } = await request.json();
+    if (!journalId || !question) {
+      return new NextResponse("Journal Id and question need to be provided", {
+        status: 400,
+      });
+    }
+    const check = await sql`SELECT * FROM journals WHERE id = ${journalId};`;
+    if (!check.rows[0]) {
+      return new NextResponse("No user", {
+        status: 400,
+      });
+    }
+
+    await sql`INSERT INTO questions (journal_id, question) VALUES (${journalId}, ${question});`;
+    return NextResponse.json(
+      { message: "Question entry created" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function GET(request: Request) {
+  try {
+    const { userId, journalId } = await request.json();
+    if (!userId) {
+      return new NextResponse("User ID not provided.", {
+        status: 400,
+      });
+    }
+
+    if (!journalId) {
+      return new NextResponse("User ID not provided.", {
+        status: 400,
+      });
+    }
+
+    const questions =
+      await sql`SELECT users.id, journals.id, question, questions.timestamp FROM users JOIN journals ON users.id = journals.user_id JOIN questions ON journals.id = questions.journal_id WHERE users.id = ${userId} AND journals.id = ${journalId};`;
+    return NextResponse.json({ questions: questions.rows }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+  }
+}
---
+++ src/app/api/responses/route.ts
@@ -0,0 +1,56 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const { questionId, answer } = await request.json();
+    if (!questionId || !answer) {
+      return new NextResponse("Journal Id and question need to be provided", {
+        status: 400,
+      });
+    }
+    const check = await sql`SELECT * FROM journals WHERE id = ${questionId};`;
+    if (!check.rows[0]) {
+      return new NextResponse("No user", {
+        status: 400,
+      });
+    }
+
+    await sql`INSERT INTO responses (question_id, answer) VALUES (${questionId}, ${answer});`;
+    return NextResponse.json(
+      { message: "Responses entry created" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function GET(request: Request) {
+  try {
+    const { userId, journalId, questionId } = await request.json();
+    if (!userId) {
+      return new NextResponse("User ID not provided.", {
+        status: 400,
+      });
+    }
+
+    if (!journalId) {
+      return new NextResponse("User ID not provided.", {
+        status: 400,
+      });
+    }
+
+    if (!questionId) {
+      return new NextResponse("User ID not provided.", {
+        status: 400,
+      });
+    }
+
+    const responses =
+      await sql`SELECT users.id, journals.id, questions.id, answer, responses.timestamp FROM users JOIN journals ON users.id = journals.user_id JOIN questions ON journals.id = questions.journal_id JOIN responses ON questions.id = responses.question_id WHERE users.id = ${userId} AND journals.id = ${journalId} AND questions.id = ${questionId};`;
+    return NextResponse.json({ responses: responses.rows }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+  }
+}
