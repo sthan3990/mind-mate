3a1fdf4e1f423774ed632270e7b89fd9e0c6a1ef
phankons <stephen.han@gmx.com>
2023-11-01 11:12:48 -0400

    Working version of Chatbot

--- src/app/api/chatbot/route.ts
+++ src/app/api/chatbot/route.ts
@@ -1,59 +1,31 @@
-import {
-  OpenAIStream,
-  StreamingTextResponse,
-  experimental_StreamData,
-} from 'ai';
 import OpenAI from 'openai';
-
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+ 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
+ 
 // IMPORTANT! Set the runtime to edge
 export const runtime = 'edge';
-
+ 
 export async function POST(req: Request) {
+  // Extract the `messages` from the body of the request
   const { messages } = await req.json();
-
+ 
+  // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo-0613',
+    model: 'gpt-3.5-turbo',
     stream: true,
-    messages,
+    temperature: 1.1,
+    max_tokens: 600,
+    top_p: 1,
+    frequency_penalty: 0.3,
+    presence_penalty: 0.5,
+    messages
   });
-
-  const data = new experimental_StreamData();
-  const stream = OpenAIStream(response, {
-    experimental_onFunctionCall: async (
-      { name, arguments: args },
-      createFunctionCallMessages,
-    ) => {
-      if (name === 'chat') {
-        // Simulate a supportive conversation
-        const userMessage = messages[messages.length - 1].content;
-        let assistantResponse = '';
-
-        if (userMessage.includes('How are you?')) {
-          assistantResponse = "I'm just a computer program, but I'm here to chat with you!";
-        } else if (userMessage.includes('Tell me a joke.')) {
-          assistantResponse = "Sure, here's one: Why did the computer catch a cold? Because it had Windows!";
-        } else {
-          assistantResponse = "I'm here to chat, so feel free to ask me anything!";
-        }
-
-        data.append({ text: assistantResponse });
-
-        return createFunctionCallMessages([{ content: assistantResponse, role: 'assistant' }]);
-      }
-    },
-    onCompletion(completion) {
-      console.log('completion', completion);
-    },
-    onFinal(completion) {
-      data.close();
-    },
-    experimental_streamData: true,
-  });
-
-  return new StreamingTextResponse(stream, {}, data);
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
 }
--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -1,106 +1,55 @@
-import React, { useState } from 'react';
-import { sendMessageToOpenAI } from './chat';
+'use client';
 
-export default function ChatPage() {
-  const [messages, setMessages] = useState<String[]>([]); 
-  const [conversation, setConversation] = useState([]);
-  const [prompt, setPrompt] = useState('You can configure the OpenAI prompt here.');
+import { useChat } from 'ai/react';
+import {
+  Flex,
+  Box,
+  Input,
+  Button,
+  Text,
+} from '@chakra-ui/react';
 
-  const handleSendMessage = () => {
-    if (input.trim() === '') return;
-  
-    setConversation([...conversation, { role: 'user', content: input }]);
-    setMessages([...messages, { text: input, user: 'user' }]);
-    setInput('');
-  };
-  
+export default function Chatbot() {
 
-  const handleChatGPTResponse = async () => {
-    if (conversation.length === 0) return;
-
-    const response = await sendMessageToOpenAI(conversation, prompt);
-    setConversation([...conversation, { role: 'assistant', content: response.choices[0].text }]);
-    setMessages([...messages, { text: response.choices[0].text, user: 'chatGPT' }];
-  };
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: '/api/chatbot', // Specify the API endpoint
+  });
 
   return (
-
-    // JSX structure remains the same
-    <div>
-      <div>
-        <input
-          type="text"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Type a message..."
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button onClick={handleSendMessage}>Send</button>
-        <button onClick={handleChatGPTResponse}>Get OpenAI Response</button>
-      </div>
-      <div>
-        {messages.map((message, index) => (
-          <div key={index}>
-            {message.user === 'user' ?
-              (
-                <div>User: {message.text}</div>
-              ) :
-              (
-                <div>ChatGPT: {message.text}</div>
-              )
-            }
-          </div>
+    <Flex
+      mx="auto"
+      w="full"
+      h="screen"
+      maxW="lg"
+      p="24"
+      flexDir="column"
+    >
+      <Box flex="1" m="auto">
+        {messages.map((m) => (
+          <Box mb="4" key={m.id}>
+            {m.role === 'user' ? 'User: ' : 'AI: '}
+            {m.content}
+          </Box>
         ))}
-      </div>
-    </div>
-
-      <Grid
-        margin="2em"
-        background={"primary.800"}
-        templateAreas={`"header header"
-                        "nav main"
-                        "footer footer"`}
-        gridTemplateRows={'80px 1fr 30px'}
-        gridTemplateColumns={'150px 1fr'}
-        h='500px'  // Increased height for chat history
-        gap='1'
-        color='blackAlpha.700'
-        fontWeight='bold'
-      >
-        <GridItem pl='2' bg='primary.800' area={'header'}>
-          <Center>
-          <Heading  as='h1' size='2xl'>Your CBT Chatbot </Heading>
-            </Center>
-        </GridItem>
-        <GridItem pl='2' bg='pink.300' area={'nav'}>
-          Chat History
-        </GridItem>
-        <GridItem pl='2' bg='green.300' area={'main'}>
-          <Box p={4}>
-            <Text fontSize="xl" fontWeight="bold">Response area</Text>
-            
-          </Box>
-        </GridItem>
-        <GridItem pl='2' bg='blue.300' area={'footer'} alignSelf="end">  {/* Adjust placement of footer */}
-          <Box p={4}>
-            <Input
-              placeholder="Type a message..."
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              
-            />
-            <Button onClick={handleSendMessage} colorScheme="blue">
-              Send
-            </Button>
-          </Box>
-        </GridItem>
-      </Grid>
- 
+      </Box>
+      <form className="flex space-x-4" onSubmit={handleSubmit}>
+        <Input
+          value={input}
+          onChange={handleInputChange}
+          placeholder="Say something..."
+          rounded="md"
+          p="2"
+          color="black"
+        />
+        <Button
+          type="submit"
+          border="2px solid white"
+          p="2"
+          rounded="md"
+        >
+          Send
+        </Button>
+      </form>
+    </Flex>
   );
 }
