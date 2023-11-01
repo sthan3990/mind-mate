fccf509dc284cac175f2e2e7bfb01f61ca8c448c
Saba Oji <sabaoji@gmail.com>
2023-11-01 19:59:44 -0400

    finished the home screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,28 +4,37 @@ import React from "react";
 import ChakraUiProvider from "@/providers/chakra-ui.provider";
 import Navbar from "./components/header";
 import Footer from "./components/footer";
-import { Flex } from "@chakra-ui/react"; // Import Flex from Chakra UI
+import { Flex } from "@chakra-ui/react";
 import { usePathname } from 'next/navigation'
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
     const registerPathname = usePathname()
-  return (
-    <html>
-      <head>{/* Add your head content here */}</head>
-      <body>
-        <ChakraUiProvider>
-          <Flex flexDirection="column" minHeight="100vh">
-            { (registerPathname != "/register" && registerPathname != "/login") && <Navbar /> }
 
-            <Flex flex="1" justify="center" align="center">
-              {children}
-            </Flex>
-            <Footer />
-          </Flex>
-        </ChakraUiProvider>
-      </body>
-    </html>
-  );
+    const rootStyles = {
+        flexDirection: "column",
+        minHeight: "100vh",
+        bgColor: "#F9F2FF", 
+        justify: "flex-start" 
+    };
+    
+
+    return (
+        <html>
+            <head>{/* Add your head content here */}</head>
+            <body>
+                <ChakraUiProvider>
+                    <Flex sx={rootStyles}>
+                        { (registerPathname != "/" && registerPathname != "/register" && registerPathname != "/login") && <Navbar /> }
+                        <Flex flexDirection="column" minHeight="100vh" justify="flex-start">
+                            {children}
+                        </Flex>
+                        <Footer />
+                    </Flex>
+                </ChakraUiProvider>
+            </body>
+        </html>
+    );
 };
 
 export default RootLayout;
+
--- src/theme/fonts.ts
+++ src/theme/fonts.ts
@@ -2,12 +2,15 @@ import { Montserrat } from "next/font/google";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 import { Poppins } from "next/font/google";
-const poppins = Poppins({weight: "200", subsets: ["latin"]})
-const poppinsHeader = Poppins({weight: "500", subsets: ["latin"]})
+const poppins = Poppins({weight: "200", subsets: ["latin"]});
+const poppinsHeader = Poppins({weight: "500", subsets: ["latin"]});
+
+import { Alegreya_Sans } from "next/font/google";
+const alegreya_Sans = Alegreya_Sans({ weight: "500", subsets: ['greek'] });
 
 /** App Fonts */
 export const fonts = {
   body: poppins.style.fontFamily,
   heading: poppinsHeader.style.fontFamily,
-
+  alternative: alegreya_Sans.style.fontFamily,
 };
