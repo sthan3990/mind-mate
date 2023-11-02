a2315844f3c259ff9c00142590023953c915b4db
NFerracuti <npferracuti@gmail.com>
2023-11-02 11:38:17 -0400

    check vercel check

--- src/app/graph/page.tsx
+++ src/app/graph/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import Papa from 'papaparse';
+// import Papa from 'papaparse';
 import PieChartComponent from './components/piechart';
 import { Text } from '@chakra-ui/react';
 
@@ -9,20 +9,21 @@ function GraphPage() {
   const [graphData, setGraphData] = useState([]);
 
   // Read the CSV file and parse the data
-  useEffect(() => {
-    Papa.parse('/graph/data.csv', {
-      header: true,
-      download: true,
-      dynamicTyping: true,
-      complete: (result) => {
+  // useEffect(() => {
+  //   Papa.parse('/graph/data.csv', {
+  //     header: true,
+  //     download: true,
+  //     dynamicTyping: true,
+  //     complete: (result) => {
 
-        setGraphData(result.data);
-      },
-    });
-  }, []);
+  //       setGraphData(result.data);
+  //     },
+  //   });
+  // }, []);
 
   return (
-     <PieChartComponent graphData={graphData} />
+    //  <PieChartComponent graphData={graphData} />
+    <PieChartComponent />
   );
 }
 
