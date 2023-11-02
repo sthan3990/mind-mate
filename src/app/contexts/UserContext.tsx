abc798c687ac22b43377e8a85a431478022045b2
William Kemeny <williamkemeny@gmail.com>
2023-11-02 12:23:15 -0400

    added logout functionality

--- src/app/contexts/UserContext.tsx
+++ src/app/contexts/UserContext.tsx
@@ -1,8 +1,14 @@
 // src/app/contexts/UserContext.ts
 
-import React, { createContext, useState, useContext, useEffect, ReactNode } from 'react';
-import { useRouter } from 'next/navigation';
-import axios from 'axios';
+import React, {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+  ReactNode,
+} from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
 interface IUserContext {
   userId: string | null;
@@ -12,8 +18,8 @@ interface IUserContext {
 
 const UserContext = createContext<IUserContext>({
   userId: null,
-  login: () => { },
-  logout: () => { },
+  login: () => {},
+  logout: () => {},
 });
 
 export const UserProvider = ({ children }: { children: ReactNode }) => {
@@ -21,7 +27,7 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
 
   useEffect(() => {
-    const storedUserId = localStorage.getItem('User');
+    const storedUserId = localStorage.getItem("User");
     if (storedUserId) {
       setUserId(storedUserId);
     }
@@ -29,11 +35,13 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
 
   const login = async (email: string, password: string) => {
     try {
-      const response = await axios.get("/api/login", { params: { email, password } });
+      const response = await axios.get("/api/login", {
+        params: { email, password },
+      });
       console.log("login response: ", response.data);
 
       if (response.data.message === "User logged in") {
-        localStorage.setItem('User', response.data.userID);
+        localStorage.setItem("User", response.data.userID);
         setUserId(response.data.userID); // Set user ID in context state
       } else {
         // Handle any other messages or errors, maybe set an error state or show a notification
@@ -41,15 +49,14 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
       }
     } catch (error) {
       // Handle errors, e.g., show an alert or set an error state
-      console.error('Login error:', error);
+      console.error("Login error:", error);
     }
   };
 
-
   const logout = () => {
-    localStorage.removeItem('User');
+    localStorage.removeItem("User");
     setUserId(null);
-    router.push('/register'); // Redirect to register on logout
+    router.push("/register"); // Redirect to register on logout
   };
 
   return (
