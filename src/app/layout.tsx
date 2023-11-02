ef10f9127b05ec765ef4fe2a41206c0b434d0070
NFerracuti <npferracuti@gmail.com>
2023-11-02 01:05:36 -0400

    add context to store userinfo globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,37 +4,38 @@ import React from "react";
 import ChakraUiProvider from "@/providers/chakra-ui.provider";
 import Navbar from "./components/header";
 import Footer from "./components/footer";
+import { UserProvider } from "./contexts/UserContext"; // Adjust the path as necessary
 import { Flex } from "@chakra-ui/react";
-import { usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-    const registerPathname = usePathname()
+    const registerPathname = usePathname();
 
     const rootStyles = {
         flexDirection: "column",
         minHeight: "100vh",
-        bgColor: "#F9F2FF", 
-        justify: "flex-start" 
+        bgColor: "#F9F2FF",
+        justify: "flex-start"
     };
-    
 
     return (
         <html>
             <head>{/* Add your head content here */}</head>
             <body>
-                <ChakraUiProvider>
-                    <Flex sx={rootStyles}>
-                        { (registerPathname != "/" && registerPathname != "/register" && registerPathname != "/login") && <Navbar /> }
-                        <Flex flexDirection="column" minHeight="100vh" justify="flex-start">
-                            {children}
+                <UserProvider> {/* Wrap your application with UserProvider */}
+                    <ChakraUiProvider>
+                        <Flex sx={rootStyles}>
+                            {(registerPathname !== "/" && registerPathname !== "/register" && registerPathname !== "/login") && <Navbar />}
+                            <Flex flexDirection="column" minHeight="100vh" justify="flex-start">
+                                {children}
+                            </Flex>
+                            <Footer />
                         </Flex>
-                        <Footer />
-                    </Flex>
-                </ChakraUiProvider>
+                    </ChakraUiProvider>
+                </UserProvider>
             </body>
         </html>
     );
 };
 
 export default RootLayout;
-
