01f91dedeccc311212c0967c8ba32254258bb578
NFerracuti <npferracuti@gmail.com>
2023-11-03 12:27:22 -0400

    refactor home page styles to styles folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,7 @@ import {
 } from "@chakra-ui/react";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { fonts } from "@/theme/fonts";
+import * as styles from './styles/homePageStyling';
 
 const theme = extendTheme({
   breakpoints: {
@@ -45,101 +46,16 @@ const Navbar = () => {
     router.push("register");
   };
   // This is the other comment
-  const logoStyle = {
-    width: ["80px", "90px", "100px", "120px"],
-    height: ["80px", "90px", "100px", "120px"],
-    position: "absolute",
-    top: "10px",
-    left: ["10px", "20px", "30px", "50px"],
-    zIndex: 2,
-  };
-
-  const linkStyle = {
-    fontWeight: "bold",
-    color: "gray.600",
-    mx: [2, 5, 6, 7],
-    fontSize: ["0.8em", "1em", "1.2em", "1.5em"],
-    ml: [4, 10, 23],
-  };
-
-  const accountButtonStyle = {
-    backgroundColor: "rgba(254, 143, 85, 0.5)",
-    width: "10%",
-    height: "53px",
-    margin: 0,
-    lineHeight: "normal",
-    fontFamily: fonts.heading,
-    fontSize: ["0.8em", "0.9em", "1em", "1.3em"],
-    boxShadow: "2px 2px 10px rgba(0,0,0,0.2)",
-  };
-
-  const dividerStyle = {
-    mt: 4,
-    bgGradient: "linear(to-r, #D0A2D1, #F9884AAD)",
-    height: "5px",
-    width: "100%",
-    border: "none",
-    boxSizing: "border-box",
-  };
-
-  const chatStyle = {
-    color: "#FF6863",
-    fontSize: ["32px", "48px", "64px", "64px"],
-    fontFamily: fonts.alternative,
-    fontWeight: "700",
-    letterSpacing: 3.2,
-    wordWrap: "break-word",
-    display: "inline",
-  };
-
-  const dotStyle = {
-    color: "#15193B",
-    fontSize: ["32px", "48px", "64px", "64px"],
-    fontFamily: fonts.alternative,
-    fontWeight: "700",
-    letterSpacing: 3.2,
-    wordWrap: "break-word",
-    display: "inline",
-    mx: 2,
-  };
-
-  const reflectStyle = {
-    color: "rgba(255, 117.79, 42.50, 0.80)",
-    fontSize: ["32px", "48px", "64px", "64px"],
-    fontFamily: fonts.alternative,
-    fontWeight: "700",
-    letterSpacing: 3.2,
-    wordWrap: "break-word",
-    display: "inline",
-  };
-
-  const measureStyle = {
-    color: "#B022AA",
-    fontSize: ["32px", "48px", "64px", "64px"],
-    fontFamily: fonts.alternative,
-    fontWeight: "700",
-    letterSpacing: 3.2,
-    wordWrap: "break-word",
-    display: "inline",
-  };
-
-  const journeyStyle = {
-    color: "#15193B",
-    fontSize: ["32px", "48px", "64px", "64px"],
-    fontFamily: fonts.alternative,
-    fontWeight: "700",
-    letterSpacing: 3.2,
-    wordWrap: "break-word",
-  };
-
-  const awarenessStyle = {
-    color: "#15193B",
-    fontSize: ["32px", "48px", "64px", "64px"],
-    fontFamily: fonts.alternative,
-    fontWeight: "700",
-    letterSpacing: 3.2,
-    wordWrap: "break-word",
-  };
+  const logoStyle = styles.logoStyle;
+  const linkStyle = styles.linkStyle;
+  const accountButtonStyle = styles.accountButtonStyle;
+  const dividerStyle = styles.dividerStyle;
+  const chatStyle = styles.chatStyle;
+  const dotStyle = styles.dotStyle;
+  const reflectStyle = styles.reflectStyle;
+  const measureStyle = styles.measureStyle;
+  const journeyStyle = styles.journeyStyle;
+  const awarenessStyle = styles.awarenessStyle;
 
   return (
     <ChakraProvider theme={theme}>
---
+++ src/app/styles/homePageStyling.ts
@@ -0,0 +1,97 @@
+import { fonts } from "@/theme/fonts";
+
+export const logoStyle = {
+  width: ["80px", "90px", "100px", "120px"],
+  height: ["80px", "90px", "100px", "120px"],
+  position: "absolute",
+  top: "10px",
+  left: ["10px", "20px", "30px", "50px"],
+  zIndex: 2,
+};
+
+export const linkStyle = {
+  fontWeight: "bold",
+  color: "gray.600",
+  mx: [2, 5, 6, 7],
+  fontSize: ["0.8em", "1em", "1.2em", "1.5em"],
+  ml: [4, 10, 23],
+};
+
+export const accountButtonStyle = {
+  backgroundColor: "rgba(254, 143, 85, 0.5)",
+  width: "10%",
+  height: "53px",
+  margin: 0,
+  lineHeight: "normal",
+  fontFamily: fonts.heading,
+  fontSize: ["0.8em", "0.9em", "1em", "1.3em"],
+  boxShadow: "2px 2px 10px rgba(0,0,0,0.2)",
+};
+
+export const dividerStyle = {
+  mt: 4,
+  bgGradient: "linear(to-r, #D0A2D1, #F9884AAD)",
+  height: "5px",
+  width: "100%",
+  border: "none",
+  boxSizing: "border-box",
+};
+
+export const chatStyle = {
+  color: "#FF6863",
+  fontSize: ["32px", "48px", "64px", "64px"],
+  fontFamily: fonts.alternative,
+  fontWeight: "700",
+  letterSpacing: 3.2,
+  wordWrap: "break-word",
+  display: "inline",
+};
+
+export const dotStyle = {
+  color: "#15193B",
+  fontSize: ["32px", "48px", "64px", "64px"],
+  fontFamily: fonts.alternative,
+  fontWeight: "700",
+  letterSpacing: 3.2,
+  wordWrap: "break-word",
+  display: "inline",
+  mx: 2,
+};
+
+export const reflectStyle = {
+  color: "rgba(255, 117.79, 42.50, 0.80)",
+  fontSize: ["32px", "48px", "64px", "64px"],
+  fontFamily: fonts.alternative,
+  fontWeight: "700",
+  letterSpacing: 3.2,
+  wordWrap: "break-word",
+  display: "inline",
+};
+
+export const measureStyle = {
+  color: "#B022AA",
+  fontSize: ["32px", "48px", "64px", "64px"],
+  fontFamily: fonts.alternative,
+  fontWeight: "700",
+  letterSpacing: 3.2,
+  wordWrap: "break-word",
+  display: "inline",
+};
+
+export const journeyStyle = {
+  color: "#15193B",
+  fontSize: ["32px", "48px", "64px", "64px"],
+  fontFamily: fonts.alternative,
+  fontWeight: "700",
+  letterSpacing: 3.2,
+  wordWrap: "break-word",
+};
+
+export const awarenessStyle = {
+  color: "#15193B",
+  fontSize: ["32px", "48px", "64px", "64px"],
+  fontFamily: fonts.alternative,
+  fontWeight: "700",
+  letterSpacing: 3.2,
+  wordWrap: "break-word",
+};
