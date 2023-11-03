ddbc056c62c334e641919e644fd1c69dab1d5e68
William Kemeny <williamkemeny@gmail.com>
2023-11-03 13:23:57 -0400

    added Steve's work

--- src/app/journal/components/formtwo.tsx
+++ src/app/journal/components/formtwo.tsx
@@ -1,112 +1,40 @@
-'use client';
+"use client";
 
 // Journal.tsx
-import React, { useState } from 'react';
-import { InputGroup, InputLeftElement, InputRightElement, Stack, Input, Box, Grid, GridItem, Button, Text } from '@chakra-ui/react';
-import { ArrowForwardIcon } from '@chakra-ui/icons';
-import debounce from 'lodash/debounce';
+import React, { useState } from "react";
+import {
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Stack,
+  Input,
+  Box,
+  Grid,
+  GridItem,
+  Button,
+  Text,
+} from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import debounce from "lodash/debounce";
+import Chatbot from "../../chatbot/page";
+import { useNumMessages } from "../../helper/numofmessages";
 
 interface FormTwoProps {
-  setJournalEntry: (entry: string) => void;
+  numQuestions: number;
   handleContinue: () => void;
 }
 
-const FormTwo: React.FC<FormTwoProps> = ({ setJournalEntry, handleContinue }) => {
+const FormTwo: React.FC<FormTwoProps> = ({ numQuestions, handleContinue }) => {
+  const { numMessages, setNumMessages } = useNumMessages();
 
-  const [tempJournal, setTempEntry] = useState('');
+  console.log("numMessages:", numMessages); // Log numMessages
 
-  // every two minutes => update 
-  const debouncedSetJournalEntry = debounce((newJournalEntry :string) => {
-    setTempEntry(newJournalEntry);
-    setJournalEntry(tempJournal);
-  }, 120000);
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newJournalEntry = event.target.value;
-    setTempEntry(newJournalEntry); // Update state immediately
-    debouncedSetJournalEntry(newJournalEntry); // Debounced update
-  };
-
-  // handle case when user presses the button
-  const onButtonClick = () => {
-    debouncedSetJournalEntry.flush();
-    setJournalEntry(tempJournal);
-  };
-
-  return (
-    <Grid
-      templateAreas={`
-      "title title"
-      "history chat"
-      "history input"
-    `}
-      gridTemplateRows="50px 1fr 0.2fr"
-      gridTemplateColumns="150px 1fr"
-      width='90vh'
-      h='70vh'
-      gap='0.0'
-    >
-      {/* TITLE */}
-      <GridItem pl="2" border="1px solid #D0A2D1" area="title" background="#2D3258">
-        <Text
-          fontFamily="Cantarell"
-          fontWeight="bold"
-          fontSize="40px"
-          color="#FFFFFF"
-          align="center"
-        >
-          Your Guided Journal
-        </Text>
-      </GridItem>
-
-      {/* HISTORY SECTION */}
-      <GridItem pl="2" background="red" area="history">
-        <Text
-          fontFamily="Inter"
-          fontWeight="regular"
-          fontSize="24px"
-          color="#000000"
-        >
-          History Section
-        </Text>
-      </GridItem>
-
-      {/* JOURNAL SECTION */}
-      <GridItem pl="2" borderStart="1px solid #D0A2D1" borderEnd="1px solid #D0A2D1" borderTop="1px solid #D0A2D1" area="chat">
-        <Text
-          fontFamily="Inter"
-          fontWeight="regular"
-          fontSize="24px"
-          color="#000000"
-        >
-          Chat Section
-        </Text>
-      </GridItem>
-
-      {/* INPUT SECTION */}
-      <GridItem pl="2" borderBottom="1px solid #D0A2D1" borderEnd="1px solid #D0A2D1" area="input">
-        <Stack spacing={4}>
-          <InputGroup>
-            <InputLeftElement w="95%">
-              <Input size='lg'
-                backgroundColor="#737AA8"
-                h="100%"
-                value={tempJournal}
-                onChange={handleInputChange}
-              />
-            </InputLeftElement>
-            <InputRightElement w="5%">
-              <Button leftIcon={< ArrowForwardIcon />} 
-               onClick={() => {
-                setJournalEntry(tempJournal);
-                onButtonClick();
-              }} />
-            </InputRightElement>
-          </InputGroup>
-        </Stack>
-      </GridItem>
-    </Grid>
-  );
+  if (numMessages < numQuestions) {
+    return <Chatbot />;
+  } else {
+    setNumMessages(0); // Reset numMessages
+    handleContinue(); // Call the function to navigate to the next page
+  }
 };
 
 export default FormTwo;
