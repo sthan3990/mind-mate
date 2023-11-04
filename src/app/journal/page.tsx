88201fdcbe98542d4294c566fd1b2200a4fbe8da
William Kemeny <williamkemeny@gmail.com>
2023-11-04 10:10:44 -0400

    Can switch between the fields of progress report and now we need to add the logic

--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -11,7 +11,6 @@ import FormFour from "./components/formfour";
 import axios from "axios";
 import WarningModal from "./components/warningmodal";
 
-
 const Journal: React.FC = () => {
   //userId from useContext
   const { userId } = useUser();
@@ -41,7 +40,7 @@ const Journal: React.FC = () => {
       day;
     setJournalId(newJournalId);
   }, [step, numQuestions, journalEntry, preMoodState, postMoodState, userId]);
-  
+
   const checkFields = () => {
     if (step === 0 && numQuestions === 0) {
       onOpen(); // Open the modal when fields are missing
@@ -66,29 +65,28 @@ const Journal: React.FC = () => {
     }
   };
   const writeToSql = (which: number) => {
-  if (which === 1) {
-    // axios
-    //   .get("/api/questions", {
-    //     params: { journalId, numQuestions }
-    //   })
-    //   .then((res) => {
-    //     console.log(res.data);
-    //   });
-  }
-  else {
-    axios
-      .post("/api/journals", {
-        userId,
-        preMoodState,
-        postMoodState,
-        numQuestions,
-      })
-      .then((res) => {
-        if (res.data.message === "Journal Created") {
-          setJournalId(res.data.journalId.id);
-        }
-      });
-  }
+    if (which === 1) {
+      // axios
+      //   .get("/api/questions", {
+      //     params: { journalId, numQuestions }
+      //   })
+      //   .then((res) => {
+      //     console.log(res.data);
+      //   });
+    } else {
+      axios
+        .post("/api/journals", {
+          userId,
+          preMoodState,
+          postMoodState,
+          numQuestions,
+        })
+        .then((res) => {
+          if (res.data.message === "Journal Created") {
+            setJournalId(res.data.journalId.id);
+          }
+        });
+    }
   };
 
   const handleBack = () => {
@@ -153,7 +151,6 @@ const Journal: React.FC = () => {
           </Box>
         )}
         <WarningModal isOpen={isOpen} step={step} onClose={onClose} />
-
       </Stack>
     </>
   );
