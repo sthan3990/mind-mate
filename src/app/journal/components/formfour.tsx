fb15e2fa87f3b6f88b4df1f455f5ba8ffe7a0bcf
phankons <stephen.han@gmx.com>
2023-11-05 15:04:00 -0500

    Upload Proper Image. Centered Motivational Quote

--- src/app/journal/components/formfour.tsx
+++ src/app/journal/components/formfour.tsx
@@ -2,7 +2,7 @@
 'use client';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { Center, SimpleGrid, Heading, Image, Stack, Box, Text } from '@chakra-ui/react';
+import { VStack, Spacer, SimpleGrid, Heading, Image, Stack, Box, Text } from '@chakra-ui/react';
 import { fonts } from "@/theme/fonts";
 import * as styles from "../../styles/journalStyle";
 import { rightSideStyle } from '@/app/styles/loginStyle';
@@ -31,12 +31,20 @@ const FormFour: React.FC = ({ }) => {
 
   return (
     <SimpleGrid 
+    bg="#F9F2FF" 
     spacing={0.1} w="full" 
     minChildWidth="320px"
     >
    
       {/* Left Side with Quote */}
-      <Stack bg="#F9F2FF" color="black" sx={leftSideStyle}>
+      <VStack 
+        bg="#F9F2FF" 
+        justifyContent="center"
+        align='stretch'
+        color="black"
+        sx={leftSideStyle}
+        spacing={2}
+        >
             
           <Box p={{ base: 4, md: 6, lg: 8 }}>
           <Heading 
@@ -46,17 +54,23 @@ const FormFour: React.FC = ({ }) => {
             &quot;{quoteMessage}&quot;
           </Heading>
         </Box>
+
         <Box>
           <Text sx={textStyle}> 
             {`By ${quoteAuthor} `}
           </Text>
         </Box>
-      </Stack>
+
+      </VStack>
 
       <Stack bg="#F9F2FF" color="black" sx={rightSideStyle}>
         <Box sx={mainImageStyle}>
           {/* Main Image */}
-          <Image src="./journal/pagefour.png" alt="Mind Mate" />
+          <Image
+            background="transparent"
+             src="/journal/pagefour.png" 
+             alt="Mind Mate"
+              />
         </Box>
         </Stack>
     </SimpleGrid>
