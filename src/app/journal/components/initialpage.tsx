c77e7d015aea8ad09ed7a8f215ac8a4e5e4b8170
NFerracuti <npferracuti@gmail.com>
2023-11-05 13:03:46 -0500

    replicate new selexct and hover on initialpage

--- src/app/journal/components/initialpage.tsx
+++ src/app/journal/components/initialpage.tsx
@@ -8,6 +8,7 @@ interface InitialJournalProps {
 
 const InitialJournal: React.FC<InitialJournalProps> = ({ setNumQuestions }) => {
   const [selectedNum, setSelectedNum] = useState<number | null>(null);
+  const [hoveredNum, setHoveredNum] = useState<number | null>(null);
 
   const handleSelectNumber = (num: number) => {
     setSelectedNum(num);
@@ -39,52 +40,54 @@ const InitialJournal: React.FC<InitialJournalProps> = ({ setNumQuestions }) => {
           Select the Number of Questions
         </Text>
       </Box>
-
-      <Box>
-        <HStack spacing="4" align="center" marginTop="40px">
-          {[1, 3, 5].map((number) => (
-            <Box key={number}>
-              <Button
-                backgroundColor="transparent"
-                onClick={() => handleSelectNumber(number)}
-                _hover={{
-                  backgroundColor: 'transparent',
-                }}
-                paddingTop="50px"
+      <HStack spacing="4" align="center" marginTop="40px">
+        {[1, 3, 5].map((number) => (
+          <Box
+            key={number}
+            onMouseEnter={() => setHoveredNum(number)}
+            onMouseLeave={() => setHoveredNum(null)}
+          >
+            <Button
+              backgroundColor="transparent"
+              onClick={() => handleSelectNumber(number)}
+              _hover={{
+                backgroundColor: 'transparent',
+              }}
+              paddingTop="50px"
+            >
+              <svg
+                width="134"
+                height="134"
+                viewBox="0 0 134 134"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <svg
-                  width="134"
-                  height="134"
-                  viewBox="0 0 134 134"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+                {/* Conditional rendering of the selection or hover circle */}
+                <circle
+                  cx="67"
+                  cy="67"
+                  r="65"
+                  fill="white"
+                  stroke={isSelected(number) ? "#FE8F55E5" : hoveredNum === number ? "#D0A2D1" : "black"}
+                  strokeWidth={isSelected(number) || hoveredNum === number ? "6" : "4"}
+                />
+                <text
+                  x="50%"
+                  y="50%"
+                  textAnchor="middle"
+                  dy=".3em"
+                  fill="black"
+                  fontSize="40px"
+                  fontWeight="semibold"
+                  fontFamily={fonts.heading}
                 >
-                  <circle
-                    cx="67"
-                    cy="67"
-                    r="65"
-                    fill="white"
-                    stroke={isSelected(number) ? "#FE8F55E5" : "black"}
-                    strokeWidth={isSelected(number) ? "6" : "4"}
-                  />
-                  <text
-                    x="50%"
-                    y="50%"
-                    textAnchor="middle"
-                    dy=".3em"
-                    fill="black"
-                    fontSize="40px"
-                    fontWeight="semibold"
-                    fontFamily={fonts.heading}
-                  >
-                    {number}
-                  </text>
-                </svg>
-              </Button>
-            </Box>
-          ))}
-        </HStack>
-      </Box>
+                  {number}
+                </text>
+              </svg>
+            </Button>
+          </Box>
+        ))}
+      </HStack>
     </Stack>
   );
 };
