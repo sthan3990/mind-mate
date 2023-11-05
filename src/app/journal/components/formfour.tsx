137dcb1757a4147b20bc0f85c45454d64f1eaf8d
phankons <stephen.han@gmx.com>
2023-11-05 14:55:22 -0500

    Fix Page Four Style and Space out Journal Back and Continue Buttons

--- src/app/journal/components/formfour.tsx
+++ src/app/journal/components/formfour.tsx
@@ -2,24 +2,21 @@
 'use client';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { SimpleGrid, Heading, Image, Stack, Box, Text } from '@chakra-ui/react';
+import { Center, SimpleGrid, Heading, Image, Stack, Box, Text } from '@chakra-ui/react';
 import { fonts } from "@/theme/fonts";
 import * as styles from "../../styles/journalStyle";
+import { rightSideStyle } from '@/app/styles/loginStyle';
 
 const FormFour: React.FC = ({ }) => {
   const [quoteAuthor, setQuoteAuthor] = useState("");
   const [quoteMessage, setQuoteMessage] = useState("");
 
-  const leftSideStyle = styles.leftSideStyle;
-  const logoStyle = styles.logoStyle;
+  const leftSideStyle = styles.mainImageStyle;
+
   const mainImageStyle = styles.mainImageStyle;
   const textStyle = styles.textStyle;
   const headingStyle = styles.headingStyle;
-  const rectangleIconStyle = styles.rectangleIconStyle;
-  const registerButtonStyle = styles.registerButtonStyle;
-  const loginButtonStyle = styles.loginButtonStyle;
-  const orTextStyle = styles.orTextStyle;
-  const lineStyle = styles.lineStyle;
+ 
 
   // Code generator 
   useEffect(() => {
@@ -33,19 +30,17 @@ const FormFour: React.FC = ({ }) => {
   }, [])
 
   return (
-    <SimpleGrid columns={[1, 1, 2, 2]} spacing={0.1} w="full" minChildWidth="320px">
-      <Stack bg="#F9F2FF" color="black">
-        <Box sx={mainImageStyle}>
-          {/* Main Image */}
-          <Image src="./register/image.svg" alt="Mind Mate" />
-        </Box>
-        </Stack>
-
-
-      {/* Right Side with Signup Form */}
-      <Stack bg="#F9F2FF" color="black">
-        <Box p={{ base: 4, md: 6, lg: 8 }}>
-          <Heading fontSize={"4xl"} 
+    <SimpleGrid 
+    spacing={0.1} w="full" 
+    minChildWidth="320px"
+    >
+   
+      {/* Left Side with Quote */}
+      <Stack bg="#F9F2FF" color="black" sx={leftSideStyle}>
+            
+          <Box p={{ base: 4, md: 6, lg: 8 }}>
+          <Heading 
+          fontSize={"4xl"} 
           fontFamily={fonts.heading}
            sx={headingStyle}>
             &quot;{quoteMessage}&quot;
@@ -57,6 +52,13 @@ const FormFour: React.FC = ({ }) => {
           </Text>
         </Box>
       </Stack>
+
+      <Stack bg="#F9F2FF" color="black" sx={rightSideStyle}>
+        <Box sx={mainImageStyle}>
+          {/* Main Image */}
+          <Image src="./journal/pagefour.png" alt="Mind Mate" />
+        </Box>
+        </Stack>
     </SimpleGrid>
   );
 };
