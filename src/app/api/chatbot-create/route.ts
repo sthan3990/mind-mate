0b9ea66d27b51d06b300396746092e55a3ee154e
William Kemeny <williamkemeny@gmail.com>
2023-11-04 20:31:23 -0400

    created chatbot-create post api

---
+++ src/app/api/chatbot-create/route.ts
@@ -0,0 +1,46 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const { userId } = await request.json();
+
+    if (!userId) {
+      return new NextResponse("User id needs to be provided", {
+        status: 400,
+      });
+    }
+    const check = await sql`SELECT * FROM users WHERE id = ${userId};`;
+    if (!check.rows[0]) {
+      return new NextResponse("No user", {
+        status: 400,
+      });
+    }
+
+    await sql`INSERT INTO cbtchatsessions (user_id) VALUES (${userId});`;
+    return NextResponse.json(
+      { message: "Chat CBT Session Created" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const userId = searchParams.get("userId");
+  try {
+    if (!userId) {
+      return new NextResponse("ID not provided.", {
+        status: 400,
+      });
+    }
+
+    const sessions =
+      await sql`SELECT users.id, timestamp FROM users JOIN cbtChatSessions ON users.id = cbtChatSessions.user_id WHERE users.id = ${userId};`;
+    return NextResponse.json({ sessions: sessions.rows }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+  }
+}
