319d12c114e8477c5e118469f43071d7e7b991b6
William Kemeny <williamkemeny@gmail.com>
2023-11-05 20:18:45 -0500

    added the bar graph page

--- src/app/progress-report/components/bargraph.tsx
+++ src/app/progress-report/components/bargraph.tsx
@@ -1,31 +1,31 @@
 "use client";
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useUser } from "@/app/contexts/UserContext";
-import { Text } from '@chakra-ui/react';
-import { BarChart, CartesianGrid, YAxis, XAxis, Bar } from 'recharts';
+import { Text } from "@chakra-ui/react";
+import { BarChart, CartesianGrid, YAxis, XAxis, Bar } from "recharts";
 
 interface BarGraphProps {
-  emotion_post: string,
-  emotion_pre: string,
-  timestamp: Date
+  emotion_post: string;
+  emotion_pre: string;
+  timestamp: Date;
 }
 
-const BarGraphComponent: React.FC<BarGraphProps> = () => {
+const BarGraphComponent: React.FC = () => {
   const [graphData, setGraphData] = useState([]);
- 
-  //const {userId } = useUser();
-  const userId = "66";
+
+  const { userId } = useUser();
 
   const fetchData = () => {
-    axios.get('/api/journals/', { params: { userId } })
-      .then(response => {
+    axios
+      .get("/api/journals/", { params: { userId } })
+      .then((response) => {
         setGraphData(response.data.journals);
       })
-      .catch(error => {
-        console.error('Error:', error);
-      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   useEffect(() => {
@@ -34,19 +34,23 @@ const BarGraphComponent: React.FC<BarGraphProps> = () => {
 
   useEffect(() => {
     <BarChart width={730} height={250} data={graphData}>
-  <CartesianGrid strokeDasharray="3 3" />
-  <XAxis dataKey="timespan" />
-  <YAxis />
-  <Bar dataKey="emotion_pre" fill="#8884d8" />
-  <Bar dataKey="emotion_post" fill="#82ca9d" />
-</BarChart>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="timespan" />
+      <YAxis />
+      <Bar dataKey="emotion_pre" fill="#8884d8" />
+      <Bar dataKey="emotion_post" fill="#82ca9d" />
+    </BarChart>;
   }, [graphData]);
 
   return (
-    <div>
-     
-    </div>
+    <BarChart width={730} height={250} data={graphData}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="timespan" />
+      <YAxis />
+      <Bar dataKey="emotion_pre" fill="#8884d8" />
+      <Bar dataKey="emotion_post" fill="#82ca9d" />
+    </BarChart>
   );
-}
+};
 
 export default BarGraphComponent;
